c5d619ffce4cafabdbdf277672addabde7c70543
Olivier Ailloud <olivier.ailloud@mikamai.com>
2026-02-19 15:25:29 +0100

    add CORS exception for local React app

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,7 +1,15 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import coffees from './data/coffees.json'
 
 const app = new Hono()
+
+app.use(
+  '/*',
+  cors({
+    origin: 'http://localhost:5173',
+  })
+)
 
 app.get('/coffees', (c) => {
   return c.json(coffees)
